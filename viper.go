3fa34ffae9f17c5da5480f070ffa6e33ebfd1d95
David Imola <david.imola@everactive.com>
2022-05-24 15:57:44 -0600

    fix imports to everactive

--- viper.go
+++ viper.go
@@ -40,14 +40,14 @@ import (
 	"github.com/spf13/cast"
 	"github.com/spf13/pflag"
 
-	"github.com/spf13/viper/internal/encoding"
-	"github.com/spf13/viper/internal/encoding/dotenv"
-	"github.com/spf13/viper/internal/encoding/hcl"
-	"github.com/spf13/viper/internal/encoding/ini"
-	"github.com/spf13/viper/internal/encoding/javaproperties"
-	"github.com/spf13/viper/internal/encoding/json"
-	"github.com/spf13/viper/internal/encoding/toml"
-	"github.com/spf13/viper/internal/encoding/yaml"
+	"github.com/everactive/viper/internal/encoding"
+	"github.com/everactive/viper/internal/encoding/dotenv"
+	"github.com/everactive/viper/internal/encoding/hcl"
+	"github.com/everactive/viper/internal/encoding/ini"
+	"github.com/everactive/viper/internal/encoding/javaproperties"
+	"github.com/everactive/viper/internal/encoding/json"
+	"github.com/everactive/viper/internal/encoding/toml"
+	"github.com/everactive/viper/internal/encoding/yaml"
 )
 
 // ConfigMarshalError happens when failing to marshal the configuration.
